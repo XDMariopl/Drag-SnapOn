85d84f1592c1fb236317bd215cae0d3e1357b726
Maris Klaiva <maris.klaiva@gmail>
2023-05-24 12:19:08 +0300

    started drag_drop script

---
+++ Assets/Scripts/Drag_drop.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class Drag_drop : MonoBehaviour, IBeginDragHandler, IDragHandler,IEndDragHandler {
+
+    public Objects objektuSkripts;
+    private CanvasGroup canvasGroup;
+    private RectTransform dragObjectRectTransform;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+       
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    void Start () {
+		canvasGroup=GetComponent<CanvasGroup>();
+        dragObjectRectTransform=GetComponent<RectTransform>();
+	}
+	
+
+}
