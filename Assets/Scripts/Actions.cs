110317088abbded147bfc3a25e33903eeadc6ff6
Maris Klaiva <maris.klaiva@gmail>
2023-06-01 09:02:39 +0300

    Learning done

--- Assets/Scripts/Actions.cs
+++ Assets/Scripts/Actions.cs
@@ -3,14 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Actions : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+	public Objects objektuskripts;
 	
-	// Update is called once per frame
+	
 	void Update () {
-		
+		if(objektuskripts.lastDraged !=null)
+		{
+			if(Input.GetKey(KeyCode.Z))
+			{
+				objektuskripts.lastDraged.GetComponent<RectTransform>().transform.Rotate(0,0,Time.deltaTime*13f);
+			}
+            if (Input.GetKey(KeyCode.X))
+            {
+                objektuskripts.lastDraged.GetComponent<RectTransform>().transform.Rotate(0, 0, -Time.deltaTime * 13f);
+            }
+			if(Input.GetKey(KeyCode.UpArrow))
+			{
+				if(objektuskripts.lastDraged.GetComponent<RectTransform>().localScale.y <=0.90f) 
+				{
+					objektuskripts.lastDraged.GetComponent<RectTransform>().transform.localScale = new Vector2(objektuskripts.lastDraged.GetComponent<RectTransform>().transform.localScale.x, objektuskripts.lastDraged.GetComponent<RectTransform>().transform.localScale.y + 0.008f);
+				}
+			}
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                if (objektuskripts.lastDraged.GetComponent<RectTransform>().localScale.y >= 0.3f)
+                {
+                    objektuskripts.lastDraged.GetComponent<RectTransform>().transform.localScale = new Vector2(objektuskripts.lastDraged.GetComponent<RectTransform>().transform.localScale.x, objektuskripts.lastDraged.GetComponent<RectTransform>().transform.localScale.y - 0.008f);
+                }
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                if (objektuskripts.lastDraged.GetComponent<RectTransform>().localScale.x <= 0.90f)
+                {
+                    objektuskripts.lastDraged.GetComponent<RectTransform>().transform.localScale = new Vector2(objektuskripts.lastDraged.GetComponent<RectTransform>().transform.localScale.x+0.008f, objektuskripts.lastDraged.GetComponent<RectTransform>().transform.localScale.y);
+                }
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                if (objektuskripts.lastDraged.GetComponent<RectTransform>().localScale.x >= 0.3f)
+                {
+                    objektuskripts.lastDraged.GetComponent<RectTransform>().transform.localScale = new Vector2(objektuskripts.lastDraged.GetComponent<RectTransform>().transform.localScale.x-0.008f, objektuskripts.lastDraged.GetComponent<RectTransform>().transform.localScale.y);
+                }
+            }
+        }
 	}
 }
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Drag and drop, snap on practice
 ## Task list
 - [x] Drag and drop objects anywhere on the play field
 - [x] Reset on incorrect area
-- [ ] change rotation on z axis
-- [ ] change height y
-- [ ] change width x
+- [x] change rotation on z axis
+- [x] change height y
+- [x] change width x
 - [x] SFX
