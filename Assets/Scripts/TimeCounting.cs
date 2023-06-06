1a6c6eb3b4f794f3e134ec7c1cb47396e0a4b11b
Maris Klaiva <maris.klaiva@gmail>
2023-06-06 14:40:19 +0300

    Start_with_time

---
+++ Assets/Scripts/TimeCounting.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCounting : MonoBehaviour {
+    public float TimeStart;
+    public Text txtBox;
+
+    void Start () 
+    {
+        txtBox.text = TimeStart.ToString();
+    }
+
+}
